b56f81a980c22a916fc15baacff6b1e00c9b8c30
cooldev900 <cooldev900@gmail.com>
2023-09-13 04:52:40 -0500

    message communication

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,4 +1,5 @@
 import reloadOnUpdate from "virtual:reload-on-update-in-background-script";
+import { ContactData } from "../popup/Popup";
 
 reloadOnUpdate("pages/background");
 
@@ -9,3 +10,11 @@ reloadOnUpdate("pages/background");
 reloadOnUpdate("pages/content/style.scss");
 
 console.log("background loaded");
+
+chrome.runtime.onMessage.addListener((request: { message: string, data?: ContactData }, sender, sendResponse) => {
+    if (request.message === 'sendMessage') {
+        const { data } = request;
+        console.log(JSON.stringify(data))
+        sendResponse({success: true})
+    }
+})
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -8,7 +8,7 @@ import Message from "./Message";
 
 export interface ContactData {
   name: string
-  phoneNumber: string 
+  phoneNumber: string
 }
 
 const Popup = () => {
@@ -16,12 +16,26 @@ const Popup = () => {
   const [contacts, setContacts] = useState<Array<ContactData>>([])
   const [messageTemplate, setMessageTemplate] = useState<string>('')
 
+  const sendMessages = () => {
+    console.log('started')
+    chrome.runtime.sendMessage({ message: 'sendMessage', data: {
+      name: 'test',
+      phoneNumber: '+1 205 626 9972'
+    } }, (response) => {
+      if (response.success) {
+        console.log('clicked')
+      } else {
+        console.log('unclicked')
+      }
+    })
+  }
+
   return (
     <div className="App bg-white">
       <header className="App-header">
-        <SelectFileButton setContacts={setContacts}/>
-        <Message setMessageTemplate={setMessageTemplate}/>
-        <button className="border-[1px] rounded-[4px] text-blue-500 p-4 mb-[15px]" onClick={() => alert(JSON.stringify({contacts, messageTemplate}))}> Send Messages </button>
+        <SelectFileButton setContacts={setContacts} />
+        <Message setMessageTemplate={setMessageTemplate} />
+        <button className="border-[1px] rounded-[4px] text-blue-500 p-4 mb-[15px]" onClick={() => sendMessages()}> Send Messages </button>
       </header>
     </div>
   );
